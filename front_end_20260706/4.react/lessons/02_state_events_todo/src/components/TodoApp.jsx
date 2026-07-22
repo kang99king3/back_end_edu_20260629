219efe4aa7b51d87ced2f57512cf70a0a6c38026
kang99king3 <kang99king3@naver.com>
2026-07-22 15:37:53 +0900

    02예제 추가

--- front_end_20260706/4.react/lessons/02_state_events_todo/src/components/TodoApp.jsx
+++ front_end_20260706/4.react/lessons/02_state_events_todo/src/components/TodoApp.jsx
@@ -4,29 +4,55 @@
 import { useState, useEffect } from 'react'
 
 function TodoApp() {
-    const [todos, setTodos] = useState([])
+    //할일 목록 저장(배열상태)
+    // const [todos, setTodos] = useState([])
+
+    //useState에 함수를 할당하면 마운트시 최초 1회 실행
+    // 그 이후부터는 값을 저장하는 기능으로 실행
+    const [todos, setTodos] = useState(() => {
+        const saved = localStorage.getItem('todos')
+        return saved ? JSON.parse(saved) : []
+    })
+
+    //입력창의 값을 저장
     const [input, setInput] = useState('')
+    // 'all', 'active', 'done'
     const [filter, setFilter] = useState('all')
 
     // TODO: STEP 3-1 — 최초 마운트 시 localStorage('todos')에서 복원하세요. (useEffect, 의존성 [])
+    // useEffect(() => {
+    //     const saved = localStorage.getItem('todos')
+    //     if (saved) setTodos(JSON.parse(saved)) // 문자열 -> JS객체로 변환
+    // }, [])
 
     // TODO: STEP 3-2 — todos가 바뀔 때마다 localStorage에 저장하세요. (useEffect, 의존성 [todos])
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos)) //배열객체 -> 문자열로 직렬화
+    }, [todos])
+
 
     // TODO: STEP 2 — 추가: 기존 배열을 복사(스프레드)하고 새 항목을 붙인 '새 배열'로 교체하세요.
     //   (새 항목: { id, text, done: false }) 그리고 입력창 비우기
     const addTodo = () => {
         if (!input.trim()) return
         // 여기에 추가 로직
+        // todos 상태에 할일 목록 추가
+        // todos = [{ id:saa, text:ddd, done: false },{ id, text, done: false }]
+        // 기존내용의 배열을 복사한뒤 새로운 내용을 추가( 복사는 spread연산자를 활용)
+        setTodos([...todos, { id: Date.now(), text: input, done: false }])
+        setInput('') //입력값 비우기
     }
 
     // TODO: STEP 2 — 삭제: id가 다른 항목만 남긴 새 배열로 교체 (filter)
-    const removeTodo = (id) => {
+    const removeTodo = (id) => { // (id) 전달받은 id는 삭제할 목록의 id
         // 삭제 로직
+        setTodos(todos.filter((t) => t.id !== id))
     }
 
     // TODO: STEP 2 — 완료 토글: 해당 id만 done을 뒤집은 새 객체로 교체 (map + 스프레드)
     const toggleTodo = (id) => {
-        // 토글 로직
+        // 토글 로직                // [{},{}..] -> t={} -> ...t -> {id:aa,text:aaa, done:true}
+        setTodos(todos.map((t) => (t.id === id ? { ...t, done: !t.done } : t)))
     }
 
     // (파생값은 제공됩니다) 화면에 보여줄 목록
