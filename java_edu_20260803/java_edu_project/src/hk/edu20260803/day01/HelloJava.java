a17eae45cbff10670a23d460dce5bfe4e4852488
kang99king3 <kang99king3@naver.com>
2026-08-06 10:55:59 +0900

    완전수 추가

--- java_edu_20260803/java_edu_project/src/hk/edu20260803/day01/HelloJava.java
+++ java_edu_20260803/java_edu_project/src/hk/edu20260803/day01/HelloJava.java
@@ -1,5 +1,7 @@
 package hk.edu20260803.day01;//파일의 폴더 구조(경로), 최상단에 위치
 
+import hk.edu20260806.day04.D1_divisor;
+
 //명명법
 // 클래스명: 파스칼
 public class HelloJava {
@@ -14,7 +16,6 @@ public class HelloJava {
     // 메서명: 카멜
     public static void main(String[] args) {
         System.out.println("Hello java!!");
-
         testMethod();
     }
 
--- java_edu_20260803/java_edu_project/src/hk/edu20260806/day04/D1_divisor.java
+++ java_edu_20260803/java_edu_project/src/hk/edu20260806/day04/D1_divisor.java
@@ -5,6 +5,13 @@ public class D1_divisor {
     public static void main(String[] args) {
         divisor(12);
         greateDivisor(10, 20);
+        lowestMultiple(2, 4);
+
+        // amicable은 non-static메서드이기때문에 객체 생성해서
+        // 객체명.메서드로 호출한다.
+        D1_divisor divisor = new D1_divisor();
+        divisor.amicable(1, 5000);
+        divisor.perfectNumber(1, 5000);
     }
 
     // 약수를 구하는 메서드
@@ -18,7 +25,7 @@ public class D1_divisor {
     }
 
     // 최대공약수
-    public static void greateDivisor(int a, int b) {
+    public static int greateDivisor(int a, int b) {
 
         // 미리 원본값을 따로 저장해두기-> 나중에 출력용으로 사용하기 위해
         // 값을 복사할때.. 기본타입은 원본을 변경하지 않는다 -> immutable한 특징
@@ -42,6 +49,43 @@ public class D1_divisor {
         }
 
         System.out.printf("%d와 %d의 최대공약수는 %d입니다.\n", tempA, tempB, a);
+        return a;// 다른 곳에서 사용해야 되니깐 최대공약수를 반환하게 만들자
     }
 
+    // 최소공배수: (a*b)/두수의 최대공약수
+    public static void lowestMultiple(int a, int b) {
+        int number = greateDivisor(a, b);
+        int result = (a * b) / number;
+        System.out.printf("%d와 %d의 최소공배수는 %d입니다.\n", a, b, result);
+    }
+
+    // 진약수의 합을 구하는 메서드
+    public int sumDivisor(int a) {
+        int sum = 0;
+        for (int i = 1; i < a; i++) {
+            if (a % i == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+
+    // 친화수 구하기
+    public void amicable(int s, int e) {
+        for (int i = s; i <= e; i++) {
+            // 220 == 284 , 6완전수 : 1 2 3 6 6==6
+            if (i != sumDivisor(i) && i == sumDivisor(sumDivisor(i))) {
+                System.out.printf("%d와 %d는 친화수 관계입니다.\n", i, sumDivisor(i));
+            }
+        }
+    }
+
+    // 완전수
+    public void perfectNumber(int s, int e) {
+        for (int i = s; i <= e; i++) {
+            if (i == sumDivisor(i)) {
+                System.out.println(i + "는 완전수입니다.");
+            }
+        }
+    }
 }
