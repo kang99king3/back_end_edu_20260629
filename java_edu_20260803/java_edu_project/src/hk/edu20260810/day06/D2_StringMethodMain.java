48bb243e405592c1b6a6d3deb554e23284204f71
kang99king3 <kang99king3@naver.com>
2026-08-10 13:51:57 +0900

    코드추가

---
+++ java_edu_20260803/java_edu_project/src/hk/edu20260810/day06/D2_StringMethodMain.java
@@ -0,0 +1,10 @@
+package hk.edu20260810.day06;
+
+public class D2_StringMethodMain {
+    public static void main(String[] args) {
+        D2_StringMethodTest st = new D2_StringMethodTest();
+        System.out.println(st.sTest01("ABCDE", 2));
+        st.sTest2("ABCDEF");
+        st.sTest04();
+    }
+}
---
+++ java_edu_20260803/java_edu_project/src/hk/edu20260810/day06/D2_StringMethodTest.java
@@ -0,0 +1,51 @@
+package hk.edu20260810.day06;
+
+public class D2_StringMethodTest {
+
+    // String 주요 메서드 연습
+    // 1. 문자 하나를 반환
+    // "문자열에서 문자 하나를 인덱스로 추출하는 기능"-> "".charAt(6) ->'문'
+    // charAt(index)
+    public String sTest01(String s, int idx) {
+        char c = s.charAt(idx);// char타입의 값 표현 -> ''
+        String ss = c + "";// 문자열로 변환
+        String ss2 = String.valueOf(c);// 문자열로 변환
+
+        // 예시) 숫자형태의 문자열->숫자형태
+        int i = Integer.parseInt("100");// "100"-> 100(정수변환)
+        return ss;
+    }
+
+    // 2.문자열에서 검색하려는 단어의 인덱스를 반환하는 기능: indexOf()
+    // "ABCD" -> "BC" 검색 -> "ABCD".indexOf("BC")
+    // 반환값은? 해당 단어의 첫번째 인덱스를 반환
+    // 종류: indexOf(), lastIndexOf() --> 차이점: 앞에서부터, 뒤에서부터 검색방향
+    // 01234-> <-43210 (X), 01234 <-
+    // indexOf("A") --> "ABCACB" --> 0 반환하고 종료
+    public void sTest2(String s) {// "ABCDEF"
+        int s1 = s.indexOf("AB");
+        int s2 = s.indexOf("C", 2);// 검색 시작 인덱스 지정
+        int s3 = s.indexOf("DE", 2, 5);// 시작인덱스와 종료 인덱스로 범위 지정해서 검색
+        int s4 = s.lastIndexOf("F");
+
+        System.out.printf("%d,%d,%d,%d\n", s1, s2, s3, s4);
+
+        // 해당 단어가 존재하는지 확인하는 용도로도 많이 사용된다.(없으면 -1반환)
+        if (s.indexOf("A") != -1) {
+            System.out.println("A가 존재합니다.");
+        }
+    }
+
+    // 3.문자열의 길이 반환: length()
+
+    // 4.문자열의 내용 변환: replace("원본","새로운 내용")
+    public void sTest04() {
+        String s = "자바프로그래밍자바웹개발자,자바스크립트";
+        s.replace("자바", "java");// 원본(s)의 내용이 바뀌지 않는다(immutable)
+        s = s.replace("자바", "java");// 다시 대입해야 변경된다.
+        System.out.println(s);
+    }
+
+    // 5.문자열을 추출하기: substring()
+    // substring(idx) , substring(sIdx,eIdx)
+}
