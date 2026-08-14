a7657deb5b6209e69e77c5335609f5a0509f1dc7
kang99king3 <kang99king3@naver.com>
2026-08-14 14:48:51 +0900

    홀수마방진코드추가

---
+++ java_edu_20260803/java_edu_project/src/hk/edu20260814/day10/D3_OddMagicSquare.java
@@ -0,0 +1,19 @@
+package hk.edu20260814.day10;
+
+public class D3_OddMagicSquare {
+
+    public int[][] magic;
+
+    public D3_OddMagicSquare() {
+        this.magic = new int[3][3];
+    }
+
+    public D3_OddMagicSquare(int n) {
+        this.magic = new int[n][n];
+    }
+
+    public void make() {
+
+    }
+
+}
