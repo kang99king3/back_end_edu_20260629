c4c28e4dbd88ab3bc33369f90bb5c975e53971f1
kang99king3 <kang99king3@naver.com>
2026-08-14 11:29:04 +0900

    animal 코드 최종 업로드

--- java_edu_20260803/java_edu_project/src/hk/edu20260814/day10/D1_AnimalMain.java
+++ java_edu_20260803/java_edu_project/src/hk/edu20260814/day10/D1_AnimalMain.java
@@ -23,5 +23,42 @@ public class D1_AnimalMain {
         // 2.부모의 타입으로 자식을 참조한다.
         D1_Tiger tiger = new D1_Tiger();
         D1_Animal animalTT = tiger;
+
+        moveAnimal(animalH);
+        moveAnimal(animalT);
+        moveAnimal(animalE);
+    }
+
+    // 자식타입의 서로 다른 여러 객체들을 참조할 수 있다.
+    // ---> (파라미터에 부모 타입을 선언하면 가능해진다.)
+    public static void moveAnimal(D1_Animal animal) {
+        // 전제조건: move() 오버라이딩
+        // 3.부모의 메서드를 호출하면 자식의 메서드가 호출된다.
+        animal.move();// 하나의 타입으로 여러 행태를 나타낼 수 있다.(다형성)
+        // animal.eat();// (X)
+    }
+
+    // 다형성을 활용하지 않았을 경우
+    public static void moveAnimal(D1_Human human) {
+        human.move();
+    }
+
+    public static void moveAnimal(D1_Eagle eagle) {
+        eagle.move();
+    }
+
+    public static void moveAnimal(D1_Tiger tiger) {
+        tiger.move();
+    }
+
+    // Object클래스를 이용하는 방법(Object는 모든 클래스에 부모)
+    public static void moveAnimal(Object obj) {
+        if (obj instanceof D1_Human) {
+            D1_Human human = (D1_Human) obj;
+            human.eat();
+        } else if (obj instanceof D1_Tiger) {
+            D1_Tiger tiger = (D1_Tiger) obj;
+            tiger.eat();
+        }
     }
 }
