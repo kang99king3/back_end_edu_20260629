2efd94da0090edbb9c491c880735e047e3d7c2a5
kang99king3 <kang99king3@naver.com>
2026-08-20 13:53:41 +0900

    card객체 구현

--- java_edu_20260803/java_edu_project/src/hk/edu20260820/day13/D2_Card.java
+++ java_edu_20260803/java_edu_project/src/hk/edu20260820/day13/D2_Card.java
@@ -11,11 +11,25 @@ public class D2_Card {
     private String card;// "◆2"
 
     public D2_Card() {
-
+        init();// 초기화하는 기능을 메서드로 구현
     }
 
     // 카드 한장을 만드는 기능: 카드 한장을 랜덤하게 생성해서 card필드에 저장하기
     public void init() {
+        // 로또 45개 숫자 -> Math.random()*45+1
+        int a = (int) (Math.random() * DECK.length);// 0~3
+        int b = (int) (Math.random() * STECK.length);// 0~12
+        card = DECK[a] + STECK[b];// "◆2"
+    }
 
+    // 은닉화(캡슐화)
+    public String getCard() {
+        return card;
+    }
+
+    // [그림+숫자]로 출력하려고 문자열 형식 정의
+    @Override
+    public String toString() {
+        return "[" + card + "]";
     }
 }
---
+++ java_edu_20260803/java_edu_project/src/hk/edu20260820/day13/D2_CardMain.java
@@ -0,0 +1,10 @@
+package hk.edu20260820.day13;
+
+public class D2_CardMain {
+
+    public static void main(String[] args) {
+        D2_Card card = new D2_Card();
+        // System.out.println(card.getCard());
+        System.out.println(card);
+    }
+}
