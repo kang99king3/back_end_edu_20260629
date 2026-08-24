0c6b49aa0b4941e740020286fad61cbb9830050d
kang99king3 <kang99king3@naver.com>
2026-08-24 10:31:38 +0900

    카드게임과제추가

---
+++ java_edu_20260803/java_edu_project/src/hk/edu20260820/day13_1/CardMain.java
@@ -0,0 +1,10 @@
+package hk.edu20260820.day13_1;
+
+public class CardMain {
+    public static void main(String[] args) {
+
+        PlayUtil util = new PlayUtil();
+        util.playGame();
+
+    }
+}
---
+++ java_edu_20260803/java_edu_project/src/hk/edu20260820/day13_1/Player.java
@@ -0,0 +1,15 @@
+package hk.edu20260820.day13_1;
+
+import java.util.ArrayList;
+
+public class Player {
+    private ArrayList<Card> player;
+
+    public Player() {
+        player = new ArrayList<Card>();
+    }
+
+    public ArrayList<Card> getPlayer() {
+        return player;
+    }
+}
