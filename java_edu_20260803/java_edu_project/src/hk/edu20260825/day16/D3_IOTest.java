86ba0e6b80d1bd33d8d9d7b7997d558a2c940167
kang99king3 <kang99king3@naver.com>
2026-08-26 11:43:17 +0900

    io코드 추가

--- java_edu_20260803/java_edu_project/src/hk/edu20260825/day16/D3_IOTest.java
+++ java_edu_20260803/java_edu_project/src/hk/edu20260825/day16/D3_IOTest.java
@@ -1,16 +1,22 @@
 package hk.edu20260825.day16;
 
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 
 public class D3_IOTest {
 
     public static void main(String[] args) {
-        test01();
+        // test01();
+        // test02();
+        // test02_2();
+        test03();
     }
 
     // 파일을 읽고 출력하기
@@ -70,6 +76,79 @@ public class D3_IOTest {
         } catch (IOException e) {
             e.printStackTrace();
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // filter: 보조스트림을 이용해서 출력하기
+    private static void test02() {
+        String s = "파일을 기록합니다.";
+        String ss = "파일을 문자단위로 기록합니다.";
+        try (
+                // data 출력하는 파이프 생성
+                OutputStream out = new FileOutputStream(
+                        "D:\\back_end_edu_20260629\\back_end_edu_20260629\\java_edu_20260803\\java_edu_project\\src\\hk\\edu20260825\\temp\\test_filter.txt");
+                OutputStreamWriter ow = new OutputStreamWriter(out, "utf-8");
+                BufferedWriter bw = new BufferedWriter(ow);) {
+
+            // 1단계: 스트림만 사용할 경우
+            // out.write(s.getBytes("utf-8")); // 문자열을 byte단위로 쪼개서 처리함
+
+            // 2단계: writer를 사용할 경우
+            // - file바이트 출력 스트림 -> 문자기반 출력 필터를 끼우고 실행(알아서 쪼개서 처리함)
+            // ow.write(ss);
+
+            // 3단계: 버퍼를 사용할 경우
+            // --> 성능향상: 문자하나마다 출력 처리 -> 많은 양의 문자를 모아서 한번에 출력(기본 8kb씩 저장해서 출력)
+            bw.write(ss);
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    // filter: 보조스트림사용
+    public static void test02_2() {
+        String s = "파일을 기록합니다.";
+        try (
+                OutputStream out = new FileOutputStream(
+                        "D:\\back_end_edu_20260629\\back_end_edu_20260629\\java_edu_20260803\\java_edu_project\\src\\hk\\edu20260825\\temp\\test_filter2.txt");
+                // filter: 기본 데이터 타입을 이진데이터로 출력(자바프로그램끼리 주고받고 처리할때 사용)
+                DataOutputStream ds = new DataOutputStream(out);) {
+            // ds.writeUTF(s);// UTF-8형식으로 인코딩된 문자열을 출력한다.
+            // ds.writeChars(s);
+            // out.write(s.getBytes());
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    // 한번에 읽을 때 크기를 설정해서 읽고 쓰기
+    public static void test03() {
+        try (
+                InputStream in = new FileInputStream(
+                        "D:\\back_end_edu_20260629\\back_end_edu_20260629\\java_edu_20260803\\java_edu_project\\src\\hk\\edu20260825\\temp\\test_img.png");
+                OutputStream out = new FileOutputStream(
+                        "D:\\back_end_edu_20260629\\back_end_edu_20260629\\java_edu_20260803\\java_edu_project\\src\\hk\\edu20260825\\temp\\test_img_copy.png");) {
+            // 10byte 단위로 읽기
+            byte[] b = new byte[10];
+            int i = 0;
+            while ((i = in.read(b)) != -1) { // i에 저장되는 값은 읽은 개수
+                // out.write(b);// b[11,12,13,14,15,6,7,8,9,10] -> 출력 파일크기 15
+                out.write(b, 0, i);// 읽은 개수만큼만 출력 -> 안정적(권장)
+            }
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
