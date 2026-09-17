671ad2475e243eb0513ddc677d23e22654d5cd91
kang99king3 <kang99king3@naver.com>
2026-09-17 14:59:48 +0900

    서블릿추가

---
+++ web_edu_20260910/02_hkboard_MVC1/src/main/webapp/error.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<h1>시스템 오류 입니다. 관리자에서 문의하세요(☎️02-1234-5678)</h1>
+<a href="index.jsp">메인으로 돌아기기</a>
+</body>
+</html>
---
+++ web_edu_20260910/03_hello_servlet/src/main/java/com/hk/hello/HelloServlet.java
@@ -0,0 +1,55 @@
+package com.hk.hello;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+//url-mapping 어노테이션 방식: tomcat7.0부터 지원
+@WebServlet("/HelloServlet.do")
+public class HelloServlet extends HttpServlet{
+
+	//init(): 서블릿 객체가 생성될때 최초 한번 실행되는 메서드
+	@Override
+	public void init() throws ServletException {
+		System.out.println("init():최초 한번 실행");
+	}
+	
+	//service(): 요청과 응답에 대한 처리 --> doGet(), doPost() 로 구현함
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse respose) throws ServletException, IOException {
+		
+		//인코딩처리
+		request.setCharacterEncoding("utf-8");
+		respose.setContentType("text/html;charset=utf-8");
+		
+		//파라미터 받기
+		String param = request.getParameter("param");
+		
+		//서블릿에서 클라이언트로 응답하기
+		PrintWriter out=respose.getWriter();
+		out.print("<h1 style='color:blue;'>서블릿개념</h1>");
+		out.print("<h2>서블릿 기본 내용 알아보기</h2>");
+		out.print("<h2>서블릿에서 받은 파라미터:"+param+"</h2>");
+		out.print("<h3><a href='index.jsp'>메인으로 돌아가기</a></h3>");
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//post방식으로 요청된 req,resp를 doGet으로 전달하면 
+		//  --> doGet에서 요청, 응답처리 할 수 있다.
+		doGet(req, resp);
+	}
+	
+	@Override
+	public void destroy() {
+		System.out.println("요청이 더이상 없으면 자동으로 서블릿 객체를 소멸시킨다.");
+	}
+}
+
+
+
