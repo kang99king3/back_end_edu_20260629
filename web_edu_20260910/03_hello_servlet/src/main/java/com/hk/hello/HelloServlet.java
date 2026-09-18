4022e970ecacbb5de3427c62ca8f87e4a75b6092
kang99king3 <kang99king3@naver.com>
2026-09-18 10:56:00 +0900

    session개념 추가

--- web_edu_20260910/03_hello_servlet/src/main/java/com/hk/hello/HelloServlet.java
+++ web_edu_20260910/03_hello_servlet/src/main/java/com/hk/hello/HelloServlet.java
@@ -4,14 +4,22 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebInitParam;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 //url-mapping 어노테이션 방식: tomcat7.0부터 지원
-@WebServlet("/HelloServlet.do")
+@WebServlet(
+		 urlPatterns = {"/HelloServlet.do"},
+		 initParams = {
+				 @WebInitParam(name="name",value="한경닷컴")
+		 }
+		)
 public class HelloServlet extends HttpServlet{
 
 	//init(): 서블릿 객체가 생성될때 최초 한번 실행되는 메서드
@@ -24,8 +32,17 @@ public class HelloServlet extends HttpServlet{
 	//  ---> init()메서드에서 파라미터를 통해 얻을 수 있다.
 	@Override
 	public void init(ServletConfig config) throws ServletException {
+		//서블릿에 정의된 초기값 가져오기
 		String name=config.getInitParameter("name");
 		System.out.println("서블릿 초기값:"+name);
+		
+		//ServletContext객체 얻어오기(JSP: application객체)
+		ServletContext application=config.getServletContext();
+		application.setAttribute("id", "hk");//객체 저장
+		System.out.println((String)application.getAttribute("id"));
+		
+		//web.xml에 정의하고 가져올 경우
+		System.out.println((String)application.getInitParameter("driver"));
 	}
 	
 	//service(): 요청과 응답에 대한 처리 --> doGet(), doPost() 로 구현함
@@ -37,7 +54,7 @@ public class HelloServlet extends HttpServlet{
 		respose.setContentType("text/html;charset=utf-8");
 		
 		//request에서 제공하는 메서드 일부 살펴보면
-		System.out.println("요청주소:"+request.getRequestURI());
+		System.out.println("요청주소:"+request.getRequestURL());
 		
 		//파라미터 받기
 		String param = request.getParameter("param");
@@ -51,6 +68,11 @@ public class HelloServlet extends HttpServlet{
 		
 		//페이지로 응답할 경우 
 //		respose.sendRedirect("index.jsp");
+		
+		//session 객체 얻어오기
+		HttpSession session = request.getSession();
+		session.setAttribute("id", "hk");
+		// --> 요청한 pc에 브라우저에 쿠키를 확인해보면 session_id
 	}
 	
 	@Override
